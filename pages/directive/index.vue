7bf4934656c50a850f7d50e176ad4d1fa9263cf2
fangweinan <1410263553@qq.com>
2024-03-29 16:12:17 +0800

    feat: volta directive

---
+++ pages/directive/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <input type="text" v-model="color" v-color:[color].ccc.ddd="color"/>
+</template>
+
+<script setup lang='ts'>
+const color = ref('red')
+
+const vColor = {
+    // mounted(el:any, binding:any,vnode:any,prevVnode:any) {
+    //     console.log('mountedmountedmountedmountedmounted',)
+    //     console.log('el',el)
+    //     console.log('binding',binding)
+    //     console.log('vnode',vnode)
+    //     console.log('prevVnode',prevVnode)
+    // },
+    updated(el:any, binding:any,vnode:any,prevVnode:any) {
+        console.log('updatedupdatedupdatedupdatedupdatedupdated',)
+        console.log('el',el)
+        console.log('binding',binding)
+        console.log('vnode',vnode)
+        console.log('prevVnode',prevVnode)
+    }
+
+}
+</script>
+<style scoped lang='less'>
+</style>
